6f1885a05ba74946b7347e87cfca183ab54f4214
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-01 17:52:12 +0000

    Automated update

--- src/Soenneker.Cohere.OpenApiClient/V1/Datasets/Item/DatasetsItemRequestBuilder.cs
+++ src/Soenneker.Cohere.OpenApiClient/V1/Datasets/Item/DatasetsItemRequestBuilder.cs
@@ -36,7 +36,7 @@ namespace Soenneker.Cohere.OpenApiClient.V1.Datasets.Item
         /// <summary>
         /// Delete a dataset by ID. Datasets are automatically deleted after 30 days, but they can also be deleted manually.
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseSchema"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
         /// <exception cref="global::Soenneker.Cohere.OpenApiClient.Models.BadRequestResponse">When receiving a 400 status code</exception>
@@ -53,11 +53,11 @@ namespace Soenneker.Cohere.OpenApiClient.V1.Datasets.Item
         /// <exception cref="global::Soenneker.Cohere.OpenApiClient.Models.GatewayTimeoutResponse">When receiving a 504 status code</exception>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public async Task<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseResponseJson?> DeleteAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseSchema?> DeleteAsync(Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #nullable restore
 #else
-        public async Task<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseResponseJson> DeleteAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
+        public async Task<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseSchema> DeleteAsync(Action<RequestConfiguration<DefaultQueryParameters>> requestConfiguration = default, CancellationToken cancellationToken = default)
         {
 #endif
             var requestInfo = ToDeleteRequestInformation(requestConfiguration);
@@ -76,7 +76,7 @@ namespace Soenneker.Cohere.OpenApiClient.V1.Datasets.Item
                 { "503", global::Soenneker.Cohere.OpenApiClient.Models.ServiceUnavailableResponse.CreateFromDiscriminatorValue },
                 { "504", global::Soenneker.Cohere.OpenApiClient.Models.GatewayTimeoutResponse.CreateFromDiscriminatorValue },
             };
-            return await RequestAdapter.SendAsync<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseResponseJson>(requestInfo, global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseResponseJson.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
+            return await RequestAdapter.SendAsync<global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseSchema>(requestInfo, global::Soenneker.Cohere.OpenApiClient.Models.DeleteDataset200ResponseSchema.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Retrieve a dataset by ID. See [&apos;Datasets&apos;](https://docs.cohere.com/docs/datasets) for more information.

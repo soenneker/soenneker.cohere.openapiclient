4e13c5e87550ced68f0c4bed2618b20338a7db00
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-28 01:13:23 +0000

    Automated update

--- src/Soenneker.Cohere.OpenApiClient/V2/V2RequestBuilder.cs
+++ src/Soenneker.Cohere.OpenApiClient/V2/V2RequestBuilder.cs
@@ -6,6 +6,7 @@ using Soenneker.Cohere.OpenApiClient.V2.Audio;
 using Soenneker.Cohere.OpenApiClient.V2.Batches;
 using Soenneker.Cohere.OpenApiClient.V2.Chat;
 using Soenneker.Cohere.OpenApiClient.V2.Embed;
+using Soenneker.Cohere.OpenApiClient.V2.Parse;
 using Soenneker.Cohere.OpenApiClient.V2.Rerank;
 using System.Collections.Generic;
 using System.IO;
@@ -38,6 +39,11 @@ namespace Soenneker.Cohere.OpenApiClient.V2
         public global::Soenneker.Cohere.OpenApiClient.V2.Embed.EmbedRequestBuilder Embed
         {
             get => new global::Soenneker.Cohere.OpenApiClient.V2.Embed.EmbedRequestBuilder(PathParameters, RequestAdapter);
+        }
+        /// <summary>The parse property</summary>
+        public global::Soenneker.Cohere.OpenApiClient.V2.Parse.ParseRequestBuilder Parse
+        {
+            get => new global::Soenneker.Cohere.OpenApiClient.V2.Parse.ParseRequestBuilder(PathParameters, RequestAdapter);
         }
         /// <summary>The rerank property</summary>
         public global::Soenneker.Cohere.OpenApiClient.V2.Rerank.RerankRequestBuilder Rerank
